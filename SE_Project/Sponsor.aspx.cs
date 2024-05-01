8310ac7ded710b584f2f4fa4a4e20dc42f3f8b7d
Hisham Hasan <hishhasanstudent@gmail.com>
2024-05-01 14:14:54 +0500

    finished Sponsor Login and Registration

--- SE_Project/Sponsor.aspx.cs
+++ SE_Project/Sponsor.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -12,13 +13,25 @@ public partial class Sponsor : System.Web.UI.Page
         if (!IsPostBack)
         {
             usernameLabel.Text = Session["username"].ToString();
-            repNameLabel.Text = Session["fullname"].ToString();
-            emailLabel.Text = Session["email"].ToString();
-            phoneLabel.Text = Session["phone"].ToString();
-            companyNameLabel.Text = Session["company"].ToString();
-            categoryLabel.Text = Session["category"].ToString();
-            packageLabel.Text = Session["package"].ToString();
-            cnicLabel.Text = Session["cnic"].ToString();
+            String connString = System.Configuration.ConfigurationManager.ConnectionStrings["WebConnString"].ToString();
+            SqlConnection conn = new SqlConnection(connString);
+
+            conn.Open();
+            string query = @"SELECT AU.username, AU.fullname, email, phone, company, cnic, category, package FROM AllUsers AU, Sponsors S WHERE AU.username = @username";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@username", usernameLabel.Text);
+
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.Read() && reader.HasRows)
+            {
+                repNameLabel.Text = reader.GetString(1);
+                emailLabel.Text = reader.GetString(2);
+                phoneLabel.Text = reader.GetString(3);
+                companyNameLabel.Text = reader.GetString(4);
+                categoryLabel.Text = reader.GetString(6);
+                packageLabel.Text = reader.GetString(7);
+                cnicLabel.Text = reader.GetString(5);
+            }
         }
     }
 }
